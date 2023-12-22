f386621dd94f1ac371b97785e2296a8d31de698d
debora-smb <debora.smborges@gmail.com>
2023-12-22 18:12:13 +0100

    add icons and logo in header, and add css style to it

--- src/app/components/header.jsx
+++ src/app/components/header.jsx
@@ -1,4 +1,5 @@
-import styles from './header.module.css';
+import styles from './header.module.css'
+import Image from 'next/image'
 
 
 export default function Header(){
@@ -9,6 +10,39 @@ export default function Header(){
                 <a href="doctors">Doctores</a>
                 <a href="contact">Contacto</a>
             </nav>
+            <Image
+                    className={styles.header__logo}
+                    src="/img/logos/logo.png"
+                    width={190}
+                    height={190}
+                    alt="Logo de Doctor Who"
+                />
+            <div className={styles.header__iconsSection}>
+                <Image
+                    src="/img/icons/facebook.svg"
+                    width={24}
+                    height={24}
+                    alt="Logo de Facebook"
+                />
+                <Image
+                    src="/img/icons/simple-icons_x.svg"
+                    width={24}
+                    height={24}
+                    alt="Logo de Twitter"
+                />
+                <Image
+                    src="/img/icons/mdi_instagram.svg"
+                    width={24}
+                    height={24}
+                    alt="Logo de Instagram"
+                />
+                <Image
+                    src="/img/icons/ic_baseline-tiktok.svg"
+                    width={24}
+                    height={24}
+                    alt="Logo de Tiktok"
+                />
+            </div>
         </header>
     )
 }
