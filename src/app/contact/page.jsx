9d1229e4683106717ca216734dd04beb4b091d8d
debora-smb <debora.smborges@gmail.com>
2023-12-22 17:32:16 +0100

    add contact page header

---
+++ src/app/contact/page.jsx
@@ -0,0 +1,12 @@
+import styles from './page.module.css'
+import Header from '../components/header'
+
+export default function Contact() {
+  return (
+    <>
+    <Header/>
+    <main className={styles.main}>
+    </main>
+    </>
+  )
+}
