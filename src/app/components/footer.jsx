e64bd2cccccdf149ad4b4f797f7c9a16952787e6
debora-smb <debora.smborges@gmail.com>
2023-12-31 17:41:18 +0100

    add burger menu

--- src/app/components/footer.jsx
+++ src/app/components/footer.jsx
@@ -6,44 +6,44 @@ export default function Footer(){
     return (
         <footer className={styles.footer}>
             <Image
-                    className={styles.footerImage}
-                    src="/img/logos/logo-cadena.png"
-                    width={30}
-                    height={30}
-                    alt="Logo Doctor Who Cadena"
-                />
+                className={styles.footerImage}
+                src="/img/logos/logo-cadena.png"
+                width={30}
+                height={30}
+                alt="Logo Doctor Who Cadena"
+            />
             <div className={styles.footerIconsSection}>
                 <a href="#">
-                <Image
-                    src="/img/icons/facebook.svg"
-                    width={20}
-                    height={20}
-                    alt="Logo de Facebook"
-                />
+                    <Image
+                        src="/img/icons/facebook.svg"
+                        width={20}
+                        height={20}
+                        alt="Logo de Facebook"
+                    />
                 </a>
                 <a href="#">
-                <Image
-                    src="/img/icons/simple-icons_x.svg"
-                    width={20}
-                    height={20}
-                    alt="Logo de Twitter"
-                />
+                    <Image
+                        src="/img/icons/simple-icons_x.svg"
+                        width={20}
+                        height={20}
+                        alt="Logo de Twitter"
+                    />
                 </a>
                 <a href="#">
-                <Image
-                    src="/img/icons/mdi_instagram.svg"
-                    width={24}
-                    height={24}
-                    alt="Logo de Instagram"
-                />
+                    <Image
+                        src="/img/icons/mdi_instagram.svg"
+                        width={24}
+                        height={24}
+                        alt="Logo de Instagram"
+                    />
                 </a>
                 <a href="#">
-                <Image
-                    src="/img/icons/ic_baseline-tiktok.svg"
-                    width={24}
-                    height={24}
-                    alt="Logo de Tiktok"
-                />
+                    <Image
+                        src="/img/icons/ic_baseline-tiktok.svg"
+                        width={24}
+                        height={24}
+                        alt="Logo de Tiktok"
+                    />
                 </a>
             </div>
         </footer>
